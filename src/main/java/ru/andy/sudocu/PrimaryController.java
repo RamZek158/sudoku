124b27c0ddbbc48afdcdac46c4a36fd8ed7b0b13
Ruslan Makarov <rmakarov@sprinterra.ru>
2024-11-06 16:04:18 +0300

    Drawing GridPane and sudokuItem

--- src/main/java/ru/andy/sudocu/PrimaryController.java
+++ src/main/java/ru/andy/sudocu/PrimaryController.java
@@ -2,8 +2,16 @@ package ru.andy.sudocu;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.geometry.Pos; 
 
 public class PrimaryController {
@@ -11,6 +19,7 @@ public class PrimaryController {
   @FXML
   private VBox primaryVBox;
   private HBox hbox;
+  private GridPane sudokuGrid;
 
   private static int SUDOKU_ITEMS_COUNT = 9; 
   private int[][] sudoku_items = new int[SUDOKU_ITEMS_COUNT][SUDOKU_ITEMS_COUNT];
@@ -23,14 +32,14 @@ public class PrimaryController {
         sudoku_items[i][j] = i+j+1;
         if(sudoku_items[i][j] > SUDOKU_ITEMS_COUNT){
           sudoku_items[i][j] = sudoku_items[i][j] - SUDOKU_ITEMS_COUNT;
-          }
         }
       }
-      displayArray(sudoku_items);
-      displaySudoku();
+    }
+    // displayArray(sudoku_items);
+    displaySudoku();
   }
 
-    // Utility method to display the 2D array
+  // Utility method to display the 2D array
   private static void displayArray(int[][] array) {
     for (int[] row : array) {
       for (int value : row) {
@@ -41,7 +50,9 @@ public class PrimaryController {
   }
 
   private void displaySudoku () {
-    drawLayout ();
+    if(hbox == null){
+      drawLayout ();
+    }
   }
 
   private void drawLayout () {
@@ -53,5 +64,53 @@ public class PrimaryController {
 
       hbox.getChildren().add(addButton);
       primaryVBox.getChildren().add(hbox);
+      drawSudokuGrid();
+  }
+
+  private void drawSudokuGrid () {
+    if(sudokuGrid == null) {
+      sudokuGrid = new GridPane();
+      sudokuGrid.setGridLinesVisible(true);
+      sudokuGrid.setId("sudokuGrid");
+      sudokuGrid.setHgap(1);
+      sudokuGrid.setVgap(1);
+
+      int rowIndex = 0;
+      int columnIndex = 0;
+
+      for (int i = 0; i < 3; i++ ) {
+        for(int j = 0; j < 3; j++ ) {
+          sudokuGrid.add(getSudocuItem(sudoku_items[i][j]), rowIndex, columnIndex);
+          rowIndex++;
+        }
+      }
+
+      rowIndex = 0;
+      columnIndex = 1;
+      for (int i = 0; i < 3; i++ ) {
+        for(int j = 3; j < 6; j++ ) {
+          sudokuGrid.add(getSudocuItem(sudoku_items[i][j]), rowIndex, columnIndex);
+          rowIndex++;
+        }
+      }
+
+      hbox.getChildren().add(sudokuGrid);
+    }
+  }
+
+
+  private StackPane getSudocuItem(int sudokuValue) {
+    Text sudokuItemTextValue = new Text(Integer.toString(sudokuValue));
+    Font font = Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 24);
+    sudokuItemTextValue.setFont(font);
+
+    Rectangle sudokuItemRectangle = new Rectangle(50, 50);
+    sudokuItemRectangle.setFill(Color.AQUA);
+    sudokuItemRectangle.setStroke(Color.BLACK);
+    sudokuItemRectangle.setStrokeWidth(1);
+
+    StackPane sudokuItem = new StackPane();
+    sudokuItem.getChildren().addAll(sudokuItemRectangle, sudokuItemTextValue);
+    return sudokuItem;
   }
 }
